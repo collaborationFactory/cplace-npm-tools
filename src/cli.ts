5b1041a75c0314466d149ff381289aa796458e82
rosem <michael_rose@gmx.de>
2017-02-03 10:55:01 +0100

    add explanation to cli

--- src/cli.ts
+++ src/cli.ts
@@ -16,6 +16,14 @@ const cli = meow(`
             Generates release notes between the two given commits (excluding <from>, including <to>).
             If <to> is not given "HEAD" is used.
             If <lang> is not given "en" is used.
+            
+            The release notes command can also be used to automatically merge two messages files in git:
+            1. Add the following section to your .git/config file or global .gitconfig:
+                [merge "cplace-msgs"]
+                    name = cplace Release Notes Messages DB merger
+                    driver = cplace-cli release-notes --merge --current %A --other %B
+            2. Make sure that the .gitattributes file in your repository contains the following line:
+                release-notes/messages_*.db merge=cplace-msgs
              
     Global options:
         --${FLAG_VERBOSE}
