e681934f38d28ec28ad4cdc65fcb9f6934273bd1
Thomas Buechner <thomas.buechner@collaboration-factory.de>
2017-05-24 10:06:33 +0200

    new version

--- src/commands/branches/BranchesCommand.ts
+++ src/commands/branches/BranchesCommand.ts
@@ -7,7 +7,7 @@ import {Global} from '../../Global';
 import * as simpleGit from 'simple-git';
 import {Repository} from '../../git/Repository';
 import {IGitRemoteBranchesAndCommits, IGitStatus} from '../../git/models';
-
+import * as os from 'os';
 export class BranchesCommand implements ICommand {
 
     public prepareAndMayExecute(params: ICommandParameters): boolean {
@@ -17,35 +17,31 @@ export class BranchesCommand implements ICommand {
     public execute(): Promise<void> {
         Global.isVerbose() && console.log('running branches command in verbose mode');
 
-        return new Promise<void>((resolve, reject) => {
-            const repo = new Repository();
-            repo.getRemoteBranchesAndCommits().then((result: IGitRemoteBranchesAndCommits[]) => {
-                console.log('result of getRemoteBranches: ', result);
-                result.forEach((branchAndCommit: IGitRemoteBranchesAndCommits) => {
-                    repo.getRemoteBranchesContainingCommit(branchAndCommit.commit).then((branches: string[]) => {
-                        console.log('branch ' + branchAndCommit.branch + ' is contained in these branches: ' + branches);
-                    });
+        const repo = new Repository();
+        const eol = os.EOL;
+
+        let dotString = 'digraph G {' + eol;
+
+        return repo.getRemoteBranchesAndCommits().then((result: IGitRemoteBranchesAndCommits[]) => {
+            const promises = result.map((branchAndCommit: IGitRemoteBranchesAndCommits) => {
+                return repo.getRemoteBranchesContainingCommit(branchAndCommit.commit).then((branches: string[]) => {
+                    const index = branches.indexOf(branchAndCommit.branch);
+                    if (index > -1) {
+                        branches.splice(index, 1);
+                    }
+                    if (branches.length > 0) {
+                        Global.isVerbose() && console.log('branch ' + branchAndCommit.branch + ' is contained in these branches: ' + branches);
+                        Global.isVerbose() && console.log('');
+                        branches.forEach((b: string) => {
+                            dotString += '    ' + branchAndCommit.branch + ' -> ' + b + ';' + eol;
+                        });
+                    }
                 });
             });
-
-            resolve();
+            return Promise.all(promises).then(() => {
+                dotString += '}';
+                console.log('dotString: ' + dotString);
+            });
         });
-
-        // return new Promise<void>((resolve, reject) => {
-        //
-        //     // /* tslint:disable:no-any*/
-        //     // git.branch([], (err, branches: any) => {
-        //     //     if (err) {
-        //     //         console.log('an error occured');
-        //     //         reject(err);
-        //     //     } else {
-        //     //         Global.isVerbose() && console.log('branches', branches);
-        //     //         Global.isVerbose() && console.log('branches.branches', branches.branches);
-        //     //         resolve(branches);
-        //     //     }
-        //     // });
-        //     //
-        //     // resolve();
-        // });
     }
 }
