0b03956fd304b42f316fd835c9f8545deaf691ae
Michael Rose <michael.rose@collaboration-factory.de>
2019-04-16 17:02:59 +0200

    fix newline replacement and trim correctly

--- src/util.ts
+++ src/util.ts
@@ -3,9 +3,11 @@
  */
 
 export function enforceNewline(text: string): string {
-    return text.replace(/[\n\r]/g, '\n');
+    return text.replace(/\r\n?/g, '\n');
 }
 
 export function makeSingleLine(text: string): string {
-    return enforceNewline(text).replace(/\s*\n\s*/g, ' ');
+    return enforceNewline(text)
+        .trim()
+        .replace(/\s*\n\s*/g, ' ');
 }
