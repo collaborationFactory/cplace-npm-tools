0f1b0f0ac97b73827669c679b5304c4ad0ece57b
stefanstadler <stefan.stadler@collaboration-factory.de>
2020-10-29 10:24:00 +0100

    added unit tests

--- src/commands/e2e/E2E.test.ts
+++ src/commands/e2e/E2E.test.ts
@@ -72,3 +72,69 @@ test('E2E detects missing Allure Reporter', async () => {
         expect(e2eCommand.isAllureReporterInstalled()).toBe(false);
     });
 });
+
+test('E2E detects WDIO Image-Comparison-Service in Dev Dependencies', async () => {
+    await withTempDirectory('e2e-allure', async (dir) => {
+        mocked(util).getPathToMainRepo.mockReturnValue(dir);
+
+        const packageJson = {
+            name: 'cplace',
+            private: true,
+            devDependencies: {
+                'wdio-image-comparison-service': '^1.14.0'
+            }
+        };
+
+        const packageJsonPath = path.join(dir, 'package.json');
+        await fs.writeFileAsync(packageJsonPath, JSON.stringify(packageJson), 'utf8');
+
+        const e2eCommand = new E2E();
+        e2eCommand.prepareAndMayExecute({});
+        expect(e2eCommand.isImageComparisonInstalled()).toBe(true);
+    });
+});
+
+test('E2E detects WDIO Image-Comparison-Service in Dependencies', async () => {
+    await withTempDirectory('e2e-allure', async (dir) => {
+        mocked(util).getPathToMainRepo.mockReturnValue(dir);
+
+        const packageJson = {
+            name: 'cplace',
+            private: true,
+            dependencies: {
+                'wdio-image-comparison-service': '^1.14.0'
+            },
+            devDependencies: {
+                'allure-commandline': '1.0.0'
+            }
+        };
+
+        const packageJsonPath = path.join(dir, 'package.json');
+        await fs.writeFileAsync(packageJsonPath, JSON.stringify(packageJson), 'utf8');
+
+        const e2eCommand = new E2E();
+        e2eCommand.prepareAndMayExecute({});
+        expect(e2eCommand.isImageComparisonInstalled()).toBe(true);
+    });
+});
+
+test('E2E detects missing WDIO Image-Comparison-Service', async () => {
+    await withTempDirectory('e2e-allure', async (dir) => {
+        mocked(util).getPathToMainRepo.mockReturnValue(dir);
+
+        const packageJson = {
+            name: 'cplace',
+            private: true,
+            dependencies: {
+                'wdio-intercept-service': '0.0.0'
+            }
+        };
+
+        const packageJsonPath = path.join(dir, 'package.json');
+        await fs.writeFileAsync(packageJsonPath, JSON.stringify(packageJson), 'utf8');
+
+        const e2eCommand = new E2E();
+        e2eCommand.prepareAndMayExecute({});
+        expect(e2eCommand.isImageComparisonInstalled()).toBe(false);
+    });
+});
