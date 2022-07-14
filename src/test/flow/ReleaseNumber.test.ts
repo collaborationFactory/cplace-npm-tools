c1354174eab267c7857fbd81c70eef681fe4eb9a
stefancplace <stefan.stadler@collaboration-factory.de>
2022-07-14 15:34:31 +0200

    extended test case with main and master branch

--- src/test/flow/ReleaseNumber.test.ts
+++ src/test/flow/ReleaseNumber.test.ts
@@ -3,6 +3,7 @@ import { ReleaseNumber } from "../../commands/flow/ReleaseNumber";
 test('can sort releases', () => {
     let releases = [];
     releases.push(ReleaseNumber.parse('main'));
+    releases.push(ReleaseNumber.parse('master'));
     releases.push(ReleaseNumber.parse('5.17'));
     releases.push(ReleaseNumber.parse('5.10'));
     releases.push(ReleaseNumber.parse('22.3'));
@@ -13,11 +14,11 @@ test('can sort releases', () => {
             return r1.compareTo(r2);
         });
 
-    expect(releases).toHaveLength(5);
+    expect(releases).toHaveLength(6);
     expect(releases[0].toString()).toBe('5.10.0')
     expect(releases[1].toString()).toBe('5.17.0')
     expect(releases[2].toString()).toBe('22.3.0')
     expect(releases[3].toString()).toBe('22.4.0')
     expect(releases[4].toString()).toBe('default')
-
+    expect(releases[5].toString()).toBe('default')
 });
